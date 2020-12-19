07d2868a9765df261b9486003cc87ec529daf69b
Taro Matsuzawa <taro@georepublic.co.jp>
2020-12-19 14:40:34 +0900

    use babel with loose option

--- index.js
+++ index.js
@@ -2,7 +2,9 @@
 import _ from './main.css'
 
 // maplibre
-const map = new mapboxgl.Map({
+import { Map } from 'maplibre-gl'
+
+const map = new Map({
   container: 'map',
   style: 'https://tile.openstreetmap.jp/styles/osm-bright/style.json',
   center: [140.084556, 36.104611],
